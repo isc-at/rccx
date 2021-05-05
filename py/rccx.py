989d19e3424130fd52be30aa06691cd7101239b7
rcemper <31236645+rcemper@users.noreply.github.com>
2021-05-05 17:25:09 +0200

    support $ commands

--- py/rccx.py
+++ py/rccx.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env  python3.6
-# Run Control Command eXecutor
+# Run Control Command eXecutor v0.0.2
 import socket
 import os
+import subprocess
+# rdir="/irisrun/repo/"
+rdir="/"
+port=6666
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(("", 6666))
+sock.bind(("", port))
 stop = "" 
 sock.listen(1)
 try: 
-    while True: 
+    while True:
+#        print("Listening on port: ",port)
         conn, addr = sock.accept()
         while True: 
             data = conn.recv(1024) 
@@ -15,9 +20,21 @@ try:
                 conn.close()
                 break
             txt = data.decode()
-            print(txt)
-            os.system(txt)
             stop = (txt == "###stop###")
+            if stop:
+                break              
+            rx = (txt[:1] == "$")
+            if rx :
+                txt=txt[1:]
+                stxt=rdir + "rccx '" + txt + "'"
+#                stxt=rdir + "rccx '" + txt + "' ; echo '***'"
+            else :
+               stxt = txt + " ; exit "            
+#              stxt = txt + " ; echo '***' ; exit "            
+#            print("==>",stxt)
+
+            subprocess.Popen(stxt,shell=True)           
+
         if stop:
             break
 
